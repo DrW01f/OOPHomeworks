9314b16dbcfbb1175008813238aef250912af5aa
Andrey <Dr_Wo1f@mail.ru>
2023-06-21 23:30:44 +0300

    homework 2 started

---
+++ homework2/Functions.java
@@ -0,0 +1,6 @@
+package homework2;
+
+public abstract class Functions {
+
+    
+}
---
+++ homework2/Main.java
@@ -0,0 +1,12 @@
+package homework2;
+/*Написать калькулятор в ООП стиле(инкапсуляция, наследованеи. 
+полиморфизм)*/
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+
+    }
+
+}
